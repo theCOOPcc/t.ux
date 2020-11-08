1910298f6802c8ce8fdb8a8374f3d7c27a79a42d
dscription <dscriptionplayground@gmail.com>
2020-11-08 13:53:16 -0500

    Combine Student and User Schema, remove Student Model

--- models/user.js
+++ models/user.js
@@ -1,28 +1,41 @@
 const mongoose = require('mongoose');
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 
 const SALT_ROUNDS = 6;
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: {type: String, required: true, lowercase: true, unique: true},
-  password: String
-}, {
-  timestamps: true
+const assignmentSchema = new Schema({
+  lessonId: { type: Schema.Types.ObjectId, ref: 'Lesson' },
+  dateStarted: Date,
+  score: Number,
+  isComplete: Boolean,
+  // recap: ,
+  // flag: ,
 });
 
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, lowercase: true, unique: true },
+    password: String,
+    assignments: [assignmentSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
 userSchema.set('toJSON', {
-  transform: function(doc, ret) {
+  transform: function (doc, ret) {
     // remove the password property when serializing doc to JSON
     delete ret.password;
     return ret;
-  }
+  },
 });
 
-userSchema.pre("save", function (next) {
+userSchema.pre('save', function (next) {
   // this will be set to the current document
   const user = this;
-  if (!user.isModified("password")) return next();
+  if (!user.isModified('password')) return next();
   // password has been changed - salt and hash it
   bcrypt.hash(user.password, SALT_ROUNDS, function (err, hash) {
     if (err) return next(err);
