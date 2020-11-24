e5811d850d37e06dca14836dffe22f0acac6e09f
dscription <dscriptionplayground@gmail.com>
2020-11-24 13:28:11 -0500

    create routing for page templates, note: urls to routes subject to change

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,11 +1,24 @@
-import React, { Component } from "react";
-import { Route, Redirect } from "react-router-dom";
-import NavBar from "../../components/NavBar/NavBar";
-import Signup from "../Signup/Signup";
-import Login from "../Login/Login";
-import authService from "../../services/authService";
-import Users from "../Users/Users";
-import "./App.css";
+import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import NavBar from '../../components/NavBar/NavBar';
+import Signup from '../Signup/Signup';
+import Login from '../Login/Login';
+import authService from '../../services/authService';
+import Users from '../Users/Users';
+import Student from '../Student/Student';
+import RecapLesson from '../RecapLesson/RecapLesson';
+import Profile from '../Profile/Profile';
+import PreviewLesson from '../PreviewLesson/PreviewLesson';
+import Lesson from '../Lesson/Lesson';
+import Landing from '../Landing/Landing';
+import Instructor from '../Instructor/Instructor';
+import CreateLesson from '../CreateLesson/CreateLesson';
+import Blog from '../Blog/Blog';
+import AdminSignup from '../AdminSignup/AdminSingup';
+import ManagerSignup from '../ManagerSignup/ManagerSignup'
+import About from '../About/About';
+
+import './App.css';
 
 class App extends Component {
   state = {
@@ -22,7 +35,7 @@ class App extends Component {
   };
 
   render() {
-    const {user} = this.state
+    const { user } = this.state;
     return (
       <>
         <NavBar user={user} handleLogout={this.handleLogout} />
@@ -59,6 +72,90 @@ class App extends Component {
           exact
           path="/users"
           render={() => (user ? <Users /> : <Redirect to="/login" />)}
+        />
+        <Route
+          exact
+          path="/student"
+          render={() => (
+            <Student />
+          )}
+        />
+        <Route
+          exact
+          path="/recap"
+          render={() => (
+            <RecapLesson />
+          )}
+        />
+        <Route
+          exact
+          path="/profile"
+          render={() => (
+            <Profile />
+          )}
+        />
+        <Route
+          exact
+          path="/preview-lesson"
+          render={() => (
+            <PreviewLesson />
+          )}
+        />
+        <Route
+          exact
+          path="/lesson"
+          render={() => (
+            <Lesson />
+          )}
+        />
+        <Route
+          exact
+          path="/landing"
+          render={() => (
+            <Landing />
+          )}
+        />
+        <Route
+          exact
+          path="/instructor"
+          render={() => (
+            <Instructor />
+          )}
+        />
+        <Route
+          exact
+          path="/create-lesson"
+          render={() => (
+            <CreateLesson />
+          )}
+        />
+        <Route
+          exact
+          path="/blog"
+          render={() => (
+            <Blog />
+          )}
+        />
+        <Route
+          exact
+          path="/admin-singup"
+          render={() => (
+            <AdminSignup />
+          )}
+        />
+        <Route
+          exact
+          path="/manager-singup"
+          render={() => (
+            <ManagerSignup />
+          )}
+        />
+        <Route
+          exact
+          path="/about"
+          render={() => (
+            <About />
+          )}
         />
       </>
     );
---
+++ src/pages/ManagerSignup/ManagerSignup.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class ManagerSignup extends Component {
+  state = {};
+  render() {
+    return <h1> Manager Signup Page</h1>;
+  }
+}
+
+export default ManagerSignup;
