a5eb57ab28cc8313dcb283c6b7913908b53d0d0e
Cory Spicer <corys4139@gmail.com>
2020-11-25 14:22:09 -0500

    lessonService export functions

--- src/services/lessonService.js
+++ src/services/lessonService.js
@@ -1,7 +1,7 @@
 import tokenService from '../services/tokenService';
 const BASE_URL = 'api/lessons/';
 
-export function create(lesson) {
+function create(lesson) {
     return fetch(BASE_URL, {
         method: 'POST',
         headers: { 'content-type': 'application/json', 'Authorization': 'Bearer' + tokenService.getToken() },
@@ -10,12 +10,12 @@ export function create(lesson) {
         .then(res => res.json());
 }
 
-export function getAll() {
+function getAll() {
     return fetch(BASE_URL, { mode: 'cors' })
     .then(res => res.json())
 }
 
-export function deleteOne(id) {
+function deleteOne(id) {
     return fetch(`${BASE_URL}${id}`, {
         method: 'DELETE',
         headers: {'Authorization': 'Bearer ' + tokenService.getToken()}
@@ -23,16 +23,24 @@ export function deleteOne(id) {
     .then(res => res.json());
 }
 
-export function getOne(lesson) {
+function getOne(lesson) {
     return fetch(`${BASE_URL}${lesson._id}`, {mode: "cors"})
     .then(res => res.json())
 }
 
-export function update(lesson) {
+function update(lesson) {
     return fetch(`${BASE_URL}${lesson._id}`, {
         method: "PUT",
         headers: {'content-type': 'application/json', 'Authorization': 'Bearer ' + tokenService.getToken()},
         body: JSON.stringify(lesson)
     }, {mode: "cors"})
     .then(res => res.json());
+}
+
+export default {
+    create,
+    getAll,
+    deleteOne,
+    getOne,
+    update
 }
