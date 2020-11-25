0abfd3e8207c8275d0485d9c277c80b4e12da430
Cory Spicer <corys4139@gmail.com>
2020-11-25 14:21:13 -0500

    group function export

--- src/services/groupService.js
+++ src/services/groupService.js
@@ -1,7 +1,7 @@
 import tokenService from '../services/tokenService';
 const BASE_URL = 'api/groups/';
 
-export function create(group) {
+function create(group) {
     return fetch(BASE_URL, {
         method: 'POST',
         headers: { 'content-type': 'application/json', 'Authorization': 'Bearer' + tokenService.getToken() },
@@ -10,12 +10,12 @@ export function create(group) {
         .then(res => res.json());
 }
 
-export function getAll() {
+function getAll() {
     return fetch(BASE_URL, { mode: 'cors' })
     .then(res => res.json())
 }
 
-export function deleteOne(id) {
+function deleteOne(id) {
     return fetch(`${BASE_URL}${id}`, {
         method: 'DELETE',
         headers: {'Authorization': 'Bearer ' + tokenService.getToken()}
@@ -23,16 +23,24 @@ export function deleteOne(id) {
     .then(res => res.json());
 }
 
-export function getOne(group) {
+function getOne(group) {
     return fetch(`${BASE_URL}${group._id}`, {mode: "cors"})
     .then(res => res.json())
 }
 
-export function update(group) {
+function update(group) {
     return fetch(`${BASE_URL}${group._id}`, {
         method: "PUT",
         headers: {'content-type': 'application/json', 'Authorization': 'Bearer ' + tokenService.getToken()},
         body: JSON.stringify(group)
     }, {mode: "cors"})
     .then(res => res.json());
+}
+
+export default {
+    create,
+    getAll,
+    deleteOne,
+    getOne,
+    update
 }
