0378d52debaca2674721e338a9852ef9bac66fed
23carnies <46233990+23carnies@users.noreply.github.com>
2020-12-21 14:25:51 -0500

    First page of login flow at 95%. Functional, some position styling off just a touch.

---
+++ src/components/TuxComponents/LoginComponents.js
@@ -0,0 +1,62 @@
+import styled, { css } from 'styled-components'
+
+export const TuxFlower = styled.img`
+	width: 140px;
+	z-index: 10;
+    position: absolute;
+    top: 100px;
+`;
+
+export const LoginBox = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 425px;
+    height: 510px;
+    background-color: var(--text-white);
+    box-shadow: var(--common-shadow);
+    border-radius: 10px;
+    position: absolute;
+    top: 200px;
+`;
+
+export const GoogleG = styled.img`
+    width: 35px;
+    height: 35px;
+    margin: 20px;
+    `;
+
+export const GoogleBox = styled.div`
+    width: 343px;
+    height: 80px;
+    border-radius: 5px;
+    background: linear-gradient(360deg, #F9F9F9 0%, rgba(255, 255, 255, 0) 100%);
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    margin: 50px 0 0 0;
+    box-shadow: var(--common-shadow);
+`;
+
+export const SignIn = styled.p`
+    font: 700 20px 'Roboto', sans-serif;
+    /* line-height: 23.44px; */
+    color: rgba(0,0,0,.54);
+`;
+
+export const Normal = styled.p`
+  font: 400 16px Helvetica, sans-serif;
+  /* line-height: 20px; */
+
+  ${props => props.or && css`
+    font-size: 20px;
+  `}
+
+  ${props => props.five00 && css`
+    font-weight: 500;
+    text-align: left;
+    margin: 0;
+  `}
+`;
+
