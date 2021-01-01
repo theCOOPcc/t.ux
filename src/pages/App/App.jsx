260aa3aabe6cbd71ec1089d67bea10f4716bdab3
dscription <dscriptionplayground@gmail.com>
2021-01-01 14:08:21 -0500

    refactor HeuristicsActivity into hooks, account for traversing each item in the deck

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,7 +3,7 @@ import { Route } from 'react-router-dom';
 import Signup from '../Signup/Signup';
 import Login from '../Login/Login';
 import User from '../User/User';
-import * as U from '../../components/TuxComponents/UniversalComponents'
+import * as U from '../../components/TuxComponents/UniversalComponents';
 import NavBar from '../../components/NavBar/NavBar';
 import CreateActivity from '../CreateActivity/CreateActivity';
 // import CreateActivityRefactor from '../CreateActivity/CreateActivityRefactor'
@@ -14,7 +14,9 @@ import IndexActivities from '../IndexActivities/IndexActivities';
 import CoryTestingGround from '../../pages/CoryTestingGround/CoryTestingGround';
 import './App.css';
 import PasswordResetRequest from '../PasswordResetRequest/PasswordResetRequest';
-import HeuristicsActivity from '../HeuristicsActivity/HeuristicsActivity'
+import HeuristicsActivity from '../HeuristicsActivity/HeuristicsActivity';
+import activityService from '../../services/activityService';
+import { activityData } from '../../SampleData/SampleData';
 
 // import ReactGA from 'react-ga';
 
@@ -25,7 +27,10 @@ import HeuristicsActivity from '../HeuristicsActivity/HeuristicsActivity'
 // })
 
 class App extends Component {
-  state = { user: authService.getUser() };
+  state = {
+    user: authService.getUser(),
+    // activityData: null
+  };
 
   handleLogout = () => {
     authService.logout();
@@ -36,20 +41,23 @@ class App extends Component {
     this.setState({ user: authService.getUser() });
   };
 
+  // async componentDidMount() {
+  //   const activityData = await activityService.getOne('5fedff0ed2cde92a7579795c')
+  //   this.setState({activityData})
+  // }
+
   render() {
     const { user } = this.state;
     return (
       <>
         <NavBar user={user} handleLogout={this.handleLogout} />
-
-
         {/* write conditional routing to proper homepage depending on user type */}
         {/* only get access to certain pages depending on user type */}
         <Route
           exact
           path="/"
           render={() => (user ? <User user={user} /> : <Landing />)}
-          />
+        />
 
         {/* // Signup & Login Routes */}
         <Route
@@ -57,21 +65,21 @@ class App extends Component {
           path="/signup"
           render={({ history }) => (
             <Signup
-            history={history}
-            handleSignupOrLogin={this.handleSignupOrLogin}
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
             />
-            )}
-            />
+          )}
+        />
         <Route
           exact
           path="/login"
           render={({ history }) => (
             <Login
-            history={history}
-            handleSignupOrLogin={this.handleSignupOrLogin}
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
             />
-            )}
-            />
+          )}
+        />
 
         {/* // General Routes */}
 
@@ -80,20 +88,25 @@ class App extends Component {
         <Route
           exact
           path="/preview-activity"
-          render={({ location }) => <PreviewActivity location={location} />} />
-        <Route 
-          exact path="/corytestingground" 
-          render={() => <CoryTestingGround />} />
+          render={({ location }) => <PreviewActivity location={location} />}
+        />
         <Route
-          exact path="/passwordresetrequest"
-          render={() => <PasswordResetRequest />} />
+          exact
+          path="/corytestingground"
+          render={() => <CoryTestingGround />}
+        />
+        <Route
+          exact
+          path="/passwordresetrequest"
+          render={() => <PasswordResetRequest />}
+        />
         {/* // Heuristics Route */}
         <Route
-          exact path="/activity/heuristics"
-          render={({history, location}) => <HeuristicsActivity />} />
+          exact
+          path="/activity/heuristics"
+          render={({ history, location }) => <HeuristicsActivity/>}
+        />
         {/* // 10 Heuristics sub-routes for sidebar */}
-
-        
       </>
     );
   }
