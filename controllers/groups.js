369414eb29434f8354518a14cb015fb45eff1850
dscription <dscriptionplayground@gmail.com>
2021-01-06 11:29:33 -0500

    create group in Manager page

--- controllers/groups.js
+++ controllers/groups.js
@@ -1,42 +1,63 @@
-const Group = require('../models/group')
+const Group = require('../models/group');
 
 module.exports = {
-    create,
-    delete: deleteOne,
-    update,
-    show,
-    index
-}
+  create,
+  delete: deleteOne,
+  update,
+  show,
+  index,
+};
 
 function create(req, res) {
-    console.log('controller function', req.body)
-    req.body.createdBy = req.user._id
-    Group.create(req.body)
-        .then(group => { res.json(group) })
-        .catch(err => { res. json(err) })
+  console.log('controller function', req.body);
+  // req.body.createdBy = req.user._id
+  Group.create(req.body)
+    .then((group) => {
+      res.json(group);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 }
 
 function deleteOne(req, res) {
-    Group.findByIdAndDelete(req.params.id)
-        .then(group => { res.json(group) })
-        .catch(err => { res.json(err) })
+  Group.findByIdAndDelete(req.params.id)
+    .then((group) => {
+      res.json(group);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 }
 
 function update(req, res) {
-    Group.findByIdAndUpdate(req.params, req.body, { new: true })
+  Group.findByIdAndUpdate(req.params, req.body, { new: true })
     // .populate('createdBy')
-        .then(group => { res.json(group) })
-        .catch(err => { res.json(err) })
+    .then((group) => {
+      res.json(group);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 }
 
 function show(req, res) {
-    Group.findById(req.params)
-        .then(group => { res.json(group) })
-        .catch(err => { res.json(err) })
+  Group.findById(req.params)
+    .then((group) => {
+      res.json(group);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 }
 
 function index(req, res) {
-    Group.findById({})
-        .then(groups => { res.json(groups) })
-        .catch(err => { res.json(err) })
+  console.log('getting groups!');
+  Group.find({})
+    .then((groups) => {
+      res.json(groups);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 }
