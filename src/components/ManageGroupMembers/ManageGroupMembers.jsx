207f80f1ad0d6a2ebb7165626985c9849927e31d
dscription <dscriptionplayground@gmail.com>
2021-01-18 13:21:40 -0500

    break out ModifyGroup into its own component and render in ManageGroupMembers page

--- src/components/ManageGroupMembers/ManageGroupMembers.jsx
+++ src/components/ManageGroupMembers/ManageGroupMembers.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import GroupUserTable from '../../components/GroupUserTable/GroupUserTable';
 import * as U from '../../components/TuxComponents/UniversalComponents';
 import CreateGroup from '../../components/CreateGroup/CreateGroup';
+import ModifyGroup from '../ModifyGroup/ModifyGroup';
 
 const ManageGroupMembers = ({
   groups,
@@ -20,9 +20,7 @@ const ManageGroupMembers = ({
       <U.ColorBlock SubGridBlue></U.ColorBlock>
       <div style={{ margin: `20px 20px 80px 40px` }}>
         <U.Heading3 bolder>Manage Group Members</U.Heading3>
-        <U.WideBtn teal onClick={() => setCreateGroup(!createGroup)}>
-          Create Group
-        </U.WideBtn>
+
         {!groups.length > 0 && (
           <CreateGroup
             newGroupName={newGroupName}
@@ -40,44 +38,16 @@ const ManageGroupMembers = ({
           />
         )}
         {groups.length > 0 && createGroup === false && (
-          <>
-            <U.FlexBox column>
-              <U.Normal twenty>Group:</U.Normal>
-              <select
-                value={selectedGroupIndex}
-                onChange={(e) => setSelectedGroupIndex(e.target.value)}
-              >
-                <option value=""></option>
-                {groups.map((group, index) => (
-                  <option key={index} value={index}>
-                    {group.name}
-                  </option>
-                ))}
-              </select>
-              {selectedGroupIndex && (
-                <>
-                  {groups[selectedGroupIndex].invited.length > 0 && (
-                    <GroupUserTable
-                      groups={groups}
-                      selectedGroupIndex={selectedGroupIndex}
-                    />
-                  )}
-                  <U.FlexBox column flexStartJC>
-                    {/* <U.AddStudents>Add Students Group</U.AddStudents> */}
-                    {/* <U.TextArea
-                  type="textarea"
-                  value={textInput}
-                  onChange={handleTextInputChange}
-                  placeholder="Enter email addresses seperated by commas (Ex: leo@gmail.com, dan@gmail.com, etc.."
-                /> */}
-                  </U.FlexBox>
-                  <U.WideBtn teal onClick={handleAddToGroup}>
-                    Add To Group
-                  </U.WideBtn>
-                </>
-              )}
-            </U.FlexBox>
-          </>
+          <ModifyGroup
+            selectedGroupIndex={selectedGroupIndex}
+            setSelectedGroupIndex={setSelectedGroupIndex}
+            groups={groups}
+            handleAddToGroup={handleAddToGroup}
+            createGroup={createGroup}
+            setCreateGroup={setCreateGroup}
+            textInput={textInput}
+            handleTextInputChange={handleTextInputChange}
+          />
         )}
       </div>
     </U.FlexBox>
