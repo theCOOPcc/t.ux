2bd286f1d0a769807274908352dd844760a2f69c
dscription <dscriptionplayground@gmail.com>
2021-01-18 14:16:01 -0500

    disable Answer buttons that were not selected

--- src/components/Answer/Answer.jsx
+++ src/components/Answer/Answer.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import * as A from '../TuxComponents/ActivitiesComponents';
 
-const Answer = ({ handleResponse, answer }) => {
+const Answer = ({ handleResponse, answer, response }) => {
   const [selection, setSelection] = useState(null);
 
   const handleSelection = (answer) => {
@@ -12,6 +12,7 @@ const Answer = ({ handleResponse, answer }) => {
     <A.SubmitQuestion
       right={selection && selection.isCorrect === true}
       wrong={selection && selection.isCorrect === false}
+      disabled={!response ? false : true}
       medium
       onClick={() => handleSelection(answer)}
     >
