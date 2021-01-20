4785873add54d6a91610e94f9475e980943cfc4d
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2021-01-20 15:37:53 -0500

    update activitybody to only show heuristics activity; import confirmationform

--- src/components/ActivityBody/ActivityBody.jsx
+++ src/components/ActivityBody/ActivityBody.jsx
@@ -3,25 +3,31 @@ import * as U from '../../components/TuxComponents/UniversalComponents';
 import Question from '../../components/Question/Question';
 import InjectHTML from '../../components/InjectHTML/InjectHTML';
 import Overview from '../../components/Overview/Overview';
-import ActivityLinks from '../ActivityLinks/ActivityLinks';
+import ConfirmationForm from '../../components/ConfirmationForm/ConfirmationForm'
 
-const ActivityBody = ({ currentModule, handleAnswers, started, links }) => {
-  return !started ? (
-    <Overview />
-  ) : (
+const ActivityBody = ({ currentModule, handleAnswers, started, sections }) => {
+  return (
     <U.Sub6ColGrid>
-      {/* <U.ColorBlock SubGridBlue></U.ColorBlock> */}
-      {currentModule.type === 'display' ? (
-        <div className="injectParent">
-          <InjectHTML markup={currentModule.contents} />
-        </div>
-      ) : (
-        <Question details={currentModule} handleAnswers={handleAnswers} />
-      )}
+      <ConfirmationForm sections={sections} currentModule={currentModule} />
     </U.Sub6ColGrid>
+    // If started is set to null/false then the overview page will be shown.
+
+    //commenting out 11-25 to check activity
+    //   !started  ? (
+    //     <Overview />
+    //   ) : (
+    //     <U.Sub6ColGrid>
+    //       {/* <U.ColorBlock SubGridBlue></U.ColorBlock> */}
+    //       {currentModule.type === 'display' ? (
+    //         <div className="injectParent">
+    //           <InjectHTML markup={currentModule.contents} />
+    //         </div>
+    //       ) : (
+    //         <Question details={currentModule} handleAnswers={handleAnswers} />
+    //       )}
+    //     </U.Sub6ColGrid>
+    //   )
   );
 };
 
 export default ActivityBody;
-
-// // !started  ? (
