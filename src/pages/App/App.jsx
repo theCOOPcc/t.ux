0f5ec50b984c915268c222134aa2045d585be979
dscription <dscriptionplayground@gmail.com>
2021-02-09 13:15:44 -0500

    wrap App.jsx in UserContextProvider and confirm that it has user data

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -17,6 +17,7 @@ import ActivityContextProvider from '../../contexts/ActivityContext';
 import ManagerContextProvider from '../../contexts/ManagerContext';
 
 // !A temporary list of activity route names and their ids. Below you will see this variable being mapped through and rendering the Activity Routes. This way when we have multiple activities the Routes will be dynamically generated, and we just have to store these properties on the activities themselves or the User object if we want to restrict the user to only seeing the activities that have been assigned to them.
+
 const activities = [{ name: 'heuristics', id: '6009f75ea00e3f38a7c65c7d' }];
 
 const App = () => {
