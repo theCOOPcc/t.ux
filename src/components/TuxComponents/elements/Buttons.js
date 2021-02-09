d842aad89a5c84b8ce720b64ee77cb172dd2211d
23carnies <46233990+23carnies@users.noreply.github.com>
2021-02-09 11:34:33 -0500

    Add buttons to Button file in organization

--- src/components/TuxComponents/elements/Buttons.js
+++ src/components/TuxComponents/elements/Buttons.js
@@ -1,7 +1,51 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { disable_btn, enable_btn, enable_hover } from '../utilities/Colors';
+import { btn_active_shadow } from '../utilities/Shadows';
+import { input_border } from '../utilities/Borders';
 
-export const Button = styled.button`
+
+// This was the "WideBtn", 280px
+export const Button280 = styled.button`
     border: none;
     border-radius: 5px;
-    
+    width: 280px;
+    padding: 12px 0;
+    margin: 17px 8px;
+    font: var(--pop-bolder);
+    background: ${enable_btn};
+    color: white;
+    cursor: pointer;
+
+    &:hover {
+        background: ${enable_hover};
+    }
+    &:active {
+        box-shadow: ${btn_active_shadow};
+    }
+
+    /* Disabled */
+    ${props => props.disabled && css`
+		background-color: ${disable_btn};
+		border: ${input_border};
+	
+		&:hover {
+			background-color: ${disable_btn};
+		}
+    `}    
+
+    /* Extra Wide 350px*/
+    ${props => props.wide350 && css`
+        width: 350px;
+    `}
+
+    ${props => props.login && css`
+        width: 343px;
+    `}
+
+    ${props => props.medium && css`
+        width: 224px;
+        font: var(--pop-thick);
+        font-size: 14px;
+        margin: 
+    `}
 `;
