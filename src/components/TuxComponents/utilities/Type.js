830a28c7d40b44c258edfe1e3f9d2ee36b757cc3
23carnies <46233990+23carnies@users.noreply.github.com>
2021-02-11 13:33:42 -0500

    Add type variables

---
+++ src/components/TuxComponents/utilities/Type.js
@@ -0,0 +1,8 @@
+export const pop_reg = '400 18px "Poppins", sans-serif';
+export const pop_ital = '400 18px "Poppins", sans-serif';
+export const pop_thick = '500 18px "Poppins", sans-serif';
+export const pop_semiBold = '600 18px "Poppins", sans-serif';
+export const pop_bolder = '700 18px "Poppins", sans-serif';
+export const pop_bold = '800 18px "Poppins", sans-serif';
+
+export const answerBtn = '500 14px "Poppins", sans-serif';
