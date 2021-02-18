45c8e8679a97fb8b6f972033c9f459701464ea9c
23carnies <46233990+23carnies@users.noreply.github.com>
2021-02-18 12:48:36 -0500

    Import Link

--- src/components/ActivityLinks/ActivityLinks.jsx
+++ src/components/ActivityLinks/ActivityLinks.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { Flex } from '../TuxComponents/utilities';
 import * as U from '../../components/TuxComponents/UniversalComponents';
