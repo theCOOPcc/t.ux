5af4eb2e801f92ccee60c10f3ff12ef323ad2961
23carnies <46233990+23carnies@users.noreply.github.com>
2021-02-18 14:30:26 -0500

    Convert Landing to in-file SC

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Button280 } from '../../components/TuxComponents/elements';
+import { Flex, pop_reg, grey_text  } from '../../components/TuxComponents/utilities';
 import * as U from '../../components/TuxComponents/UniversalComponents'
 
 
@@ -8,20 +11,43 @@ import * as U from '../../components/TuxComponents/UniversalComponents'
 const Landing = (props) => {
     return (
       <React.Fragment>
-        <U.FlexBox wide tuxStyled column >
-                <img src='/images/vertical-logo.svg' alt='tux-logo' />
-            <U.Normal twenty4 greyed>
-                A new way to help power the UX Learning Experience
-            </U.Normal>
-            <a href='http://www.thecoop.cc'>
-                <U.WideBtn teal>More Information</U.WideBtn>
-            </a>
-            <br></br>
-            <U.Normal twenty4 greyed>Coming Soon!</U.Normal>
-        </U.FlexBox>
+        <Box>
+          <img src='/images/vertical-logo.svg' alt='tux-logo' />
+          <Text>
+            A new way to help power the UX Learning Experience
+          </Text>
+          <a href='http://www.thecoop.cc'>
+            <Button280>More Information</Button280>
+          </a>
+          <br></br>
+          <Text>Coming Soon!</Text>
+        </Box>
       </React.Fragment>
     )
 }
 
 export default Landing;
  
+const Box = styled.div`
+    ${Flex({ai:'center',jc:'center',fd:'column'})};
+    background: linear-gradient(
+          210.65deg,
+          rgba(255, 238, 153, 0.32) 17.3%,
+          rgba(122, 218, 222, 0.32) 87.56%
+        ),
+        linear-gradient(
+          19.08deg,
+          rgba(234, 74, 70, 0.32) -33.26%,
+          rgba(234, 74, 70, 0) 67.74%
+        );
+      background-blend-mode: normal, multiply;
+      height: 100vh;
+
+`;
+
+const Text = styled.p`
+    font: ${pop_reg};
+    line-height: 20px;
+    font-size: 24px;
+    color: ${grey_text};
+`;
