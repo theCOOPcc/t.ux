e9faf6248f59fad635a837c5d7a32989d19e6a51
Cory <corys4139@gmail.com>
2021-03-30 14:09:45 -0400

    add gallery page

---
+++ src/pages/Gallery/Gallery.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Gallery = () => {
+	return (
+		<>
+			<h1>The Coop Component Gallery</h1>
+			<h3>Image tile goes here</h3>
+		</>
+	);
+};
+
+export default Gallery;
