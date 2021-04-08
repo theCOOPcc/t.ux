35f99b478d8ab28353bd924808cb9a4ed4681ac8
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2021-04-08 15:50:46 -0400

    add map function to show all components

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { GalleryComponentProps, GalleryComponent } from "./GalleryComponent";
 
-const Gallery = () => {
+const Gallery = ({ components }) => {
 	return (
 		<>
 			<h1>The Coop Component Gallery</h1>
-			<h3>Image tile goes here</h3>
+			<div>
+				{
+					(components.length > 0) && components.map((galleryComponent, idx) => (
+						<>
+							<GalleryComponent key={idx} kind={galleryComponent.kind} props={galleryComponent.props.src, galleryComponent.props.alt} />
+						</>
+
+					))
+				}
+			</div>
 		</>
 	);
 };
