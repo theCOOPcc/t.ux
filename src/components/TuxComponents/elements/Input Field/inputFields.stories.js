74fc9326c702cf7b9f9ac19e429f70045a13a000
pohern <ohern_p@yahoo.com>
2021-05-27 08:57:46 -0500

    Add Input Fields with label

---
+++ src/components/TuxComponents/elements/Input Field/inputFields.stories.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Input, Wrapper, Label, Error, ErrorMessage } from "./inputFields";
+import { withDesign } from "storybook-addon-designs";
+
+export default {
+  title: "Input Fields",
+  decorators: [withDesign],
+};
+
+export const InputFields = (args) => (
+  <Wrapper {...args}>
+    {args.error && (
+      <>
+        <Label {...args}>
+          <Error {...args} src='/images/ErrorIcon.png' />
+        </Label>
+      </>
+    )}
+    <Input
+      {...args}
+      placeholder={
+        args.focus === true
+          ? "Input Text"
+          : args.active === true
+          ? "Input Text"
+          : args.error === true
+          ? "Wrong Input"
+          : null
+      }
+    />
+
+    <Label {...args}>Label</Label>
+    {args.error &&
+    (<ErrorMessage>Error Message</ErrorMessage>)
+    }
+  </Wrapper>
+);
+
+
+
+InputFields.args = {
+  disabled: false,
+  depressed: false,
+  focus: false,
+  hover: false,
+  active: false,
+  error: false
+};
+InputFields.story = {
+  parameters: {
+    design: {
+      type: "figma",
+      url:
+        "https://www.figma.com/file/VV3mFbPYwfmOpuWfGND4Dv/Tux-Design-Library-FROZEN?node-id=228%3A85",
+    },
+  },
+};
