683590289d0028e83df859d003461858d0975820
Jowel <rjowel@gmail.com>
2021-06-21 14:09:43 -0400

    Name and style working

--- src/components/TuxComponents/elements/Avatars/Avatars.js
+++ src/components/TuxComponents/elements/Avatars/Avatars.js
@@ -1,22 +1,55 @@
 import styled, { css } from "styled-components";
 
-export const AvatarLargeBox = styled.img`
+export const AvatarLargeBox = styled.div`
+width: 60px;
+height: 60px;
+${(props) =>
+        props.name &&
+        css`
+        display:flex;
+flex-direction: column;
+        `}
+`
+export const AvatarLargeIcon = styled.img`
 width: 60px;
 height: 60px;
 `
 
-
-export const AvatarSmallBox = styled.img`
+export const AvatarSmallBox = styled.div`
+width: 40px;
+height: 40px;
+${(props) =>
+        props.name &&
+        css`
+    display:flex;
+    `}
+`
+export const AvatarSmallIcon = styled.img`
 width: 40px;
 height: 40px;
 `
 
-export const AvatarName = styled.img`
+export const AvatarLargeName = styled.div`
+position: absolute;
 width: 30px;
 height: 26px;
-font: 600 14px "Poppins", sans-serif; 
-
+margin-left: 1em;
+margin-top: 70px;
+font-family: Poppins;
+font-style: normal;
+font-weight: 600;
+font-size: 17px;
 `
-
+export const AvatarSmallName = styled.div`
+position: static;
+width: 30px;
+height: 26px;
+margin-right: 1em;
+margin-top: 10px;
+font-family: Poppins;
+font-style: normal;
+font-weight: 600;
+font-size: 17px;
+`
 
 
--- src/components/TuxComponents/elements/Avatars/Avatars.stories.js
+++ src/components/TuxComponents/elements/Avatars/Avatars.stories.js
@@ -1,4 +1,4 @@
-import { AvatarLargeBox, AvatarName } from './Avatars.js'
+import { AvatarLargeBox, AvatarLargeName, AvatarSmallName, AvatarLargeIcon, AvatarSmallBox, AvatarSmallIcon } from './Avatars.js'
 import { withDesign } from "storybook-addon-designs";
 import LargeAvatar from '../../../../images/icons/avatarLarge.svg'
 import SmallAvatar from '../../../../images/icons/avatarSmall.svg'
@@ -8,15 +8,32 @@ export default {
     decorators: [withDesign],
 };
 
-export const LAvatar = () => (
-    <img src={LargeAvatar} />
+export const LAvatar = (args) => (
+    <>
+        <AvatarLargeBox {...args} >
+            <AvatarLargeIcon className="large-avatar" src={LargeAvatar} />
+           <AvatarLargeName> {args.name===true ? 'Leo':''} </AvatarLargeName>
+        </AvatarLargeBox>
+    </>
 
+);
 
-)
+LAvatar.args = {
+    name: false
+};
 
-export const SmAvatar = () => (
-    <img src={SmallAvatar} />
-)
+export const SmAvatar = (args) => (
+    <>
+        <AvatarSmallBox {...args} >
+            <AvatarSmallName> {args.name===true ? 'Leo' : ''} </AvatarSmallName>
+            <AvatarSmallIcon className="small-avatar" src={SmallAvatar} />
+        </AvatarSmallBox>
+    </>
+);
+
+SmAvatar.args = {
+    name: false
+};
 
 
 
