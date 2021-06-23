a860e1b5984831752ee0303e557e5220e831a81c
Ben Stein <whereyouben@gmail.com>
2021-06-23 13:05:44 -0400

    unit test and styling/storybook tweaks

--- src/components/TuxComponents/elements/ToolTips/ToolTips.js
+++ src/components/TuxComponents/elements/ToolTips/ToolTips.js
@@ -1,5 +1,6 @@
 import styled, { css } from "styled-components";
 
+//flexbox used for aligning arrows and boxes
 export const ToolTip = styled.div`
   display: flex;
   align-items: center;
@@ -23,48 +24,47 @@ export const ToolTip = styled.div`
     `
   }
 `
-
+//css triangles used for arrows
 export const Arrow = styled.div`
   width: 0;
   height: 0;
   ${(props) =>
     props.left &&
     css`
-      border-top: 8px solid transparent;
-      border-bottom: 8px solid transparent;
-      border-right: 8px solid red;
+      border-top: 6px solid transparent;
+      border-bottom: 6px solid transparent;
+      border-right: 10px solid black;
     `
   }
   ${(props) =>
     props.right &&
     css`
-      border-top: 8px solid transparent;
-      border-bottom: 8px solid transparent;
-      border-left: 8px solid red;
+      border-top: 6px solid transparent;
+      border-bottom: 6px solid transparent;
+      border-left: 10px solid black;
     `
   }
   ${(props) =>
     props.top &&
     css`
-      border-left: 8px solid transparent;
-      border-right: 8px solid transparent;
-      border-bottom: 8px solid red;
+      border-left: 6px solid transparent;
+      border-right: 6px solid transparent;
+      border-bottom: 10px solid black;
     `
   }
   ${(props) =>
     props.bottom &&
     css`
-      border-left: 8px solid transparent;
-      border-right: 8px solid transparent;
-      border-top: 8px solid red;
+      border-left: 6px solid transparent;
+      border-right: 6px solid transparent;
+      border-top: 10px solid black;
     `
   }
 `
-
+//box for text
 export const Box = styled.div`
-  display: inline-block;
   background: black;
   color: white;
-  padding: 5px;
+  padding: 8px;
   border-radius: 3px;
 `
--- src/components/TuxComponents/elements/ToolTips/ToolTips.stories.js
+++ src/components/TuxComponents/elements/ToolTips/ToolTips.stories.js
@@ -3,37 +3,59 @@ import React from 'react';
 import { withDesign } from "storybook-addon-designs";
 
 export default {
-  title: "Tooltips",
+  title: "ToolTips",
   decorators: [withDesign], 
 };
 
-
 export const ToolTips = (args) => (
-  <div>     
-    <ToolTip {...args}>
+  //toggle display
+  // <div style={{float: 'left'}}>     
+  //   <ToolTip {...args}>
+  //     <Box className="test-class">
+  //       This is a tooltip
+  //     </Box>
+  //     <Arrow {...args} />
+  //   </ToolTip>
+  // </div>
+
+  //sequential display
+  <div style={{float: 'left'}}>
+    <ToolTip top={true}>
+      <Box className="test-class">
+        This is a tooltip
+      </Box>
+      <Arrow top={true} />
+    </ToolTip>
+    <br/>
+    <ToolTip left={true}>
       <Box>
         This is a tooltip
       </Box>
-      <Arrow {...args} />
+      <Arrow left={true} />
     </ToolTip>
     <br/>
-    <br/>
-    <br/>
-    <ToolTip {...args}>
+    <ToolTip bottom={true}>
       <Box>
-        This is a much longer tooltip for testing
+        This is a tooltip
       </Box>
-      <Arrow {...args} />
+      <Arrow bottom={true} />
+    </ToolTip>
+    <br/>
+    <ToolTip right={true}>
+      <Box>
+        This is a tooltip
+      </Box>
+      <Arrow right={true} />
     </ToolTip>
   </div>
 )
 
-ToolTips.args = {
-  top: false,
-  bottom: true,
-  left: false,
-  right: false
-};
+// ToolTips.args = {
+//   top: false,
+//   bottom: true,
+//   left: false,
+//   right: false
+// };
 
 ToolTips.story = {
   parameters: {
@@ -44,4 +66,3 @@ ToolTips.story = {
     },
   },
 };
-
