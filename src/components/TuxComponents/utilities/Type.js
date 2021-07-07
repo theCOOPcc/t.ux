8a8bfc80fc83f55a1211ae37c4faee301c69a851
Tyler Reid <Tylerreid1988@gmail.com>
2021-07-07 11:58:28 -0400

    TUX-236-Activity_Wizard: Organize Types.js by font size, Create PopSaveBtn font, and PopHeaderBtn font.

--- src/components/TuxComponents/utilities/Type.js
+++ src/components/TuxComponents/utilities/Type.js
@@ -1,13 +1,22 @@
+export const Poppins = '"Poppins", sans-serif';
+
 export const pop_reg = '400 18px "Poppins", sans-serif';
 export const pop_ital = 'italic 400 18px "Poppins", sans-serif';
-export const pop_thick = '500 18px "Poppins", sans-serif';
-export const pop_semiBold = '600 18px "Poppins", sans-serif';
-export const pop_bolder = '700 18px "Poppins", sans-serif';
-export const pop_bold = '800 18px "Poppins", sans-serif';
-export const pop_black = '900 20px "Poppins", sans-serif;'
 
+export const pop_thick = '500 18px "Poppins", sans-serif';
 export const answerBtn = '500 14px "Poppins", sans-serif';
-export const Poppins = '"Poppins", sans-serif';
+export const PopHeader = '500 36px "Poppins", sans-serif';
+
+export const pop_semiBold = '600 18px "Poppins", sans-serif';
+
+export const PopSaveBtn = '700 20px "Poppins", sans-serif'
+export const pop_bolder = '700 18px "Poppins", sans-serif';
+
+export const pop_bold = '800 18px "Poppins", sans-serif';
+
+export const pop_black = '900 20px "Poppins", sans-serif;';
+
+
 
 // use the font property in css and change size as needed
 // font: ${pop)reg};
