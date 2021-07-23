2eba9be73b714bec31d3846b49b6996ed6cf2cd3
sournachos <joserivera18100@gmail.com>
2021-07-23 14:01:17 -0500

    TUX_242_Contrast_Ratio_Box Started creating a rough draft for the Contrast Ratio Activity.

--- src/components/TuxComponents/elements/ColorSlider/ContrastRatio.js
+++ src/components/TuxComponents/elements/ColorSlider/ContrastRatio.js
@@ -1,29 +1,31 @@
 import styled from "styled-components";
+import { pop_bolder } from '../../utilities/Type'
+
+export const ContrastRatioActivity = styled.div`
+        // The bottom layer where the rest of the components will go
+
+        padding: 15px 30px;
+        display: flex;
+        flex-direction: column;
+        height: 400px;
+        width: 765px;
+        background-color: purple; // the hex color from input goes here
+        
+`;
 
 
 export const ContrastRatioBox = styled.div`
         // Here goes the contrast ratio on its own
-        
-        width: 128px;
+       
+        width: 103px;
         height: 67px;
-`;
+        background-color: red; // the result of the ratio test will determine the color
 
+`;        
 
-
-
-export const ContrastRatioActivity = styled.div`
-        // The bottom layer where the rest of the components will go
-        
-        display: flex;
-        flex-direction: column;
-        height: 460px;
-        width: 795px;
-        background-color: purple; // the hex color from input goes here
-
-        `;
-
-// export const BackgroundColor = styled(ContrastRatioActivity)`
-//         // Here goes the contrast ratio on its own
+// export const StandardRow = styled.div`
+//         // A foundational row to build the activity on
         
 //         background-color: #
+
 // `;     
