c78c6050b3c100dbe35b4d5414c1a56e39cae800
erikacolette <erikatidwell.c@gmail.com>
2021-08-16 12:44:49 -0400

    TUX-237 tool bra elements all done now hover state

--- src/components/TuxComponents/elements/Text_Toolbar/Text_toolbar.js
+++ src/components/TuxComponents/elements/Text_Toolbar/Text_toolbar.js
@@ -1,39 +1,67 @@
-import React from 'react';
-import styled, { css } from 'styled-components';
-
+import React from "react";
+import styled, { css } from "styled-components";
 
 export const TextToolBar = () => {
-
-    return(
-<>
-<BarContainer>
-    <B></B>
-    <I></I>
-</BarContainer>
-</>
-    )
-}
-
-
+  return (
+    <>
+      <BarContainer>
+        <B><HB></HB></B>
+        <I></I>
+        <Linkicon></Linkicon>
+        <BrokenLink></BrokenLink>
+        <MobileNav></MobileNav>
+      </BarContainer>
+    </>
+  );
+};
 
 export const BarContainer = styled.div`
-height: 50px;
-width: 240px;
-border-radius: 5px;
-background-color: #666666;
-display: flex;
-justify-content: center;
-align-items: center;
-
+  height: 50px;
+  width: 240px;
+  border-radius: 5px;
+  background-color: #666666;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 `;
 
 export const B = styled.img`
-heightL 16px;
+  height: 16px;
+  width: 12px;
+
+
+  ${(props) =>
+    props.hover &&
+    css`
+   padding: 3px 5px;
+    border: 1px solid #f1f2eb;
+    
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    `}
+ 
+`;
+export const HB = styled.img`
+height: 16px;
 width: 12px;
 `
 
 export const I = styled.img`
-heightL 16px;
-width: 12px;
+  height: 16px;
+  width: 12px;
+`;
+export const Linkicon = styled.img`
+heightL 17.76px;
+width: 19.03px;
 
-`
+`;
+
+export const BrokenLink = styled.img`
+heightL 21px;
+width: 21px;
+
+`;
+export const MobileNav = styled.img`
+heightL 15px;
+width: 18px;
+
+`;
--- src/components/TuxComponents/elements/Text_Toolbar/Text_toolbar.stories.js
+++ src/components/TuxComponents/elements/Text_Toolbar/Text_toolbar.stories.js
@@ -1,37 +1,50 @@
 import React from "react";
 import { withDesign } from "storybook-addon-designs";
-import {BarContainer, B, I} from "./Text_toolbar"
-import Bicon from "../../../../images/icons/Bicon.svg"
-import Iicon from "../../../../images/icons/Iicon.svg"
+import {
+  BarContainer,
+  B,
+  I,
+  Linkicon,
+  BrokenLink,
+  MobileNav,
+  HB,
+} from "./Text_toolbar";
+import Bicon from "../../../../images/icons/Bicon.svg";
+import Iicon from "../../../../images/icons/Iicon.svg";
+import LinkIcon from "../../../../images/icons/Link.svg";
+import BrokeLink from "../../../../images/icons/BrokenLink.svg";
+import mobilenav from "../../../../images/icons/mobilenav.svg";
+import BlueB from "../../../../images/icons/BlueB.svg";
 
 export default {
-    title: "TextToolBar",
-    decorators: [withDesign],
-  };
+  title: "TextToolBar",
+  decorators: [withDesign],
+};
 
-  export const TextToolBar = () => {
+export const TextToolBar = (args) => {
+  return (
+    <>
+      <BarContainer>
+        <B onMouseOver={e => e.currentTarget.src = BlueB}onMouseOut={e => e.currentTarget.src = Bicon} {...args} src={Bicon}></B>
+        <I src={Iicon}></I>
+        <Linkicon src={LinkIcon}></Linkicon>
+        <BrokenLink src={BrokeLink}></BrokenLink>
+        <MobileNav src={mobilenav}></MobileNav>
+      </BarContainer>
+    </>
+  );
+};
 
-    return(
-        <>
-        <BarContainer>
-            <B src={Bicon}></B>
-            <I src={Iicon}></I>
+TextToolBar.args={
+  hover: false,
+};
 
-        </BarContainer>
-        </>
-    )
-
-  }
-
-
-  TextToolBar.story = {
-    parameters: {
-      design: {
-        type: "figma",
-        url:
-          "https://www.figma.com/file/VV3mFbPYwfmOpuWfGND4Dv/Tux-Design-Library-FROZEN?node-id=1252%3A1",
-      },
+TextToolBar.story = {
+  parameters: {
+    design: {
+      type: "figma",
+      url:
+        "https://www.figma.com/file/VV3mFbPYwfmOpuWfGND4Dv/Tux-Design-Library-FROZEN?node-id=1252%3A1",
     },
-  };
-
-
+  },
+};
