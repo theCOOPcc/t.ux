7f77878fa9dec78aa9084edc96bbe34d59dfac3f
sournachos <joserivera18100@gmail.com>
2021-09-10 12:44:12 -0500

    TUX_258_Drag_And_Drops  Created files for DnD (Drag and Drop)

---
+++ src/components/TuxComponents/elements/DragAndDrop/FillBlank.stories.js
@@ -0,0 +1,7 @@
+import React, {useState} from 'react'
+import { withDesign } from "storybook-addon-designs";
+
+export default {
+  title: "Fill In The Blank Drag&Drop",
+  decorators: [withDesign],
+};
---
+++ src/components/TuxComponents/elements/DragAndDrop/TShape.stories.js
@@ -0,0 +1,7 @@
+import React, {useState} from 'react'
+import { withDesign } from "storybook-addon-designs";
+
+export default {
+  title: "T Shape Drag&Drop",
+  decorators: [withDesign],
+};
