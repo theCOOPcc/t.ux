332d76114eecdd785ba53f72a512102ba6160efd
Adam Reed <thirdeyeadam@gmail.com>
2021-09-13 15:05:09 -0500

    TUX_258_BasicFillInBlankComponent

--- src/components/TuxComponents/elements/DragAndDrop/FillBlank.stories.js
+++ src/components/TuxComponents/elements/DragAndDrop/FillBlank.stories.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
 import { withDesign } from "storybook-addon-designs";
+import { UIExampleCard, ButtonFieldContainer, CheckMarkContainer, ContentMessageHeader, ContentMessageDescription, CheckMarkImage, EmptyDragNDrop, ContinueButtonStatic, ContinueText } from './FillBlank';
+import CheckIcon from '../../../../images/DragNDrop/CheckmarkIcon.svg';
 
 export default {
   title: "Drag&Drop",
@@ -7,7 +9,29 @@ export default {
 };
 
 export const FillBlankDragnDrop = () => (
-    <>
-    
-    </>
+  <>
+    <UIExampleCard>
+      <CheckMarkContainer>
+      <CheckMarkImage src={CheckIcon}/>
+      </CheckMarkContainer>
+      <ContentMessageHeader>
+      Successfully Published
+      </ContentMessageHeader>
+      <ContentMessageDescription>
+      Your profile has been published successfully. <br></br>You can browse projects now.
+      </ContentMessageDescription>
+      <ButtonFieldContainer>
+        <EmptyDragNDrop>
+
+        </EmptyDragNDrop>
+        <ContinueButtonStatic>
+        <ContinueText>
+          Continue
+        </ContinueText>
+        </ContinueButtonStatic>
+      </ButtonFieldContainer>
+    </UIExampleCard>
+  </>
 );
+
+
