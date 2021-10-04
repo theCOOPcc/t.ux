4c34c1dfb1a791e06bbf2e90d238c782b8e97bb4
bg-write <bradywgerber@gmail.com>
2021-10-04 12:17:17 -0400

    TUX-272 Update color variables in Landing

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { PrimaryButton } from '../../components/TuxComponents/elements';
-import { Flex, pop_reg, grey_text  } from '../../components/TuxComponents/utilities';
+import { Flex, pop_reg, grey_text, backgroundGradient  } from '../../components/TuxComponents/utilities';
 
 
 // Going to be the TUX landing page.
@@ -29,16 +29,7 @@ export default Landing;
  
 const Box = styled.div`
     ${Flex({ai:'center',jc:'center',fd:'column'})};
-    background: linear-gradient(
-          210.65deg,
-          rgba(255, 238, 153, 0.32) 17.3%,
-          rgba(122, 218, 222, 0.32) 87.56%
-        ),
-        linear-gradient(
-          19.08deg,
-          rgba(234, 74, 70, 0.32) -33.26%,
-          rgba(234, 74, 70, 0) 67.74%
-        );
+    background: ${backgroundGradient};
       background-blend-mode: normal, multiply;
       height: 100vh;
 
