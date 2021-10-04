09adda7329d672f1e133b139b7f0bba2d458fe61
bg-write <bradywgerber@gmail.com>
2021-10-04 12:17:09 -0400

    TUX-272 Update color variables in HelpPage

--- src/pages/HelpPage/HelpPage.jsx
+++ src/pages/HelpPage/HelpPage.jsx
@@ -4,7 +4,7 @@ import MotivationBox from '../../components/TuxComponents/elements/MotiviationBo
 import HelpCard from '../../components/TuxComponents/elements/HelpCard/HelpCard';
 import { ColorBlock } from '../../components/TuxComponents/elements/ColorBlock/ColorBlock';
 import { PageBackground } from '../../components/TuxComponents/elements/PageBackgrounds/PageBackgrounds';
-import { pop_semiBold } from '../../components/TuxComponents/utilities';
+import { pop_semiBold, white } from '../../components/TuxComponents/utilities';
 import { BackButton } from '../../components/TuxComponents/elements/PageBackgrounds/PageBackgrounds';
 
 // title,description
@@ -78,7 +78,7 @@ export default HelpPage;
 const Main = styled.main`
 margin: 0 auto;
 max-width: 1440px;
-  background-color: var(--white);
+  background-color: ${white};
   border-radius: 5px;
   box-shadow: 0px 4px 9px rgb(0 0 0 / 25%);
   height: 62%;
