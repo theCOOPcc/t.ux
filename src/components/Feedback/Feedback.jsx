4129c5d5b7ca01c1184bab6d59f96f913852b8e2
bg-write <bradywgerber@gmail.com>
2021-10-04 12:06:05 -0400

    TUX-272 Update color variables in Feedback

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import { Flex } from '../TuxComponents/utilities';
+import { Flex, true_white, pop_bolder, pop_reg } from '../TuxComponents/utilities';
 import { JustCorrect, JustIncorrect } from '../TuxComponents/elements/Alerts/Justifications';
-// import { FlagFeature } from '../TuxComponents/elements/FlagFeature/FlagFeature';
-
 
 export const Feedback = ({ response }) => {
   const { selection, isCorrect } = response;
-  // const { isCorrect } = selection;
 
   return (
     <>
-      {/* {(isCorrect && <FeedbackBox selection={selection} />) ||
-        (!isCorrect && <FeedbackBox selection={selection} />)} */}
-               {/* {(isCorrect && <JustCorrect selection={selection} />) ||
-        (!isCorrect && <JustIncorrect selection={selection} />)}  */}
         {
           isCorrect ? <JustCorrect selection={selection} isCorrect={isCorrect} />
           : <JustIncorrect selection={selection} isCorrect={isCorrect} />
         }
-        {/* <JustCorrect selection={selection} isCorrect={isCorrect}/> */}
     </>
   );
 };
@@ -42,50 +34,20 @@ const Hint = styled.div`
     height: 65px;
     width: 100px;
     background-color: ${(props) => (props.isCorrect ? 'Green' : 'Red')};
-    color: var(--true-white);
-    font: var(--pop-bolder);
+    color: ${true_white};
+    font: ${pop_bolder};
     ${Flex({ai:'center',jc:'center'})};
     margin: 20px 0 0 65px;
-    /* flex-grow: 3; */
 `;
 
 const Tips = styled.p`
-    font: var(--pop-bolder);
+    font: ${pop_bolder};
     margin: 0;
     padding: 25px;
     text-align: left;
 `;
 
 const Text = styled.p`
-  font: var(--pop-reg);
+  font: ${pop_reg};
   line-height: 20px;
 `;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// {
-//   isCorrect ? 
-//   <JustCorrect selection={selection}/>
-//   :
-//   <JustIncorrect selection={selection}/>
-// }
-
-      /* {(isCorrect && <JustCorrect selection={selection} />) ||
-        (!isCorrect && <JustIncorrect selection={selection} />)} */
